672d31c76424979a4fba93643618d1703a7fbc83
gluttony3 <kaminskijbogdan39@gmail.com>
2025-07-26 13:05:41 +0000

    Delete json directory

--- json/o.py
+++
@@ -1,8 +0,0 @@
-import json
-with open("i.json", "r") as file:
-    data = json.load(file)
-data ["products"].append ("цукор")
-
-with open ("i.json", "w") as file:
-    json.dump(data,file, ensure_ascii=False)
-print(data)
